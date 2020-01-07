0c38ad71bc51ba7eb16bc30bf344907f7d03ee99
Y. Jung <gilsang9808@gmail.com>
2020-01-07 16:37:02 +0900

    user/membership model, controllers, and routes (#5)
    * user model
    * membership model
    * user controllers
    * user create/find on mypage
    * restore to db on membership register

---
+++ controllers/users.ts
@@ -0,0 +1,18 @@
+import { UserModel } from '../models/user';
+import { MembershipModel } from '../models/membership';
+
+export async function findUser(plusfriendUserKey: string) {
+    return await UserModel.findOne({"plusfriendUserKey": plusfriendUserKey});
+}
+
+export async function addUser(plusfriendUserKey: string) {
+    console.log( await UserModel.create({"plusfriendUserKey": plusfriendUserKey}) );
+    return true;
+}
+
+export async function askRegister(plusfriendUserKey: string, imageUrl: string, reqType: string = 'shy') {
+    const user = await UserModel.findOne({"plusfriendUserKey": plusfriendUserKey});
+    if(!user) throw Error("user doesn't exists");
+    user.requests.push({imageUrl: imageUrl, type: reqType});
+    await user.save();
+}
